c8fcce5671b547fb3fa50141776ed08ffaf48900
alexanderlamoureux@hotmail.com <alexanderlamoureux@hotmail.com>
2017-05-15 16:41:53 -0700

    going through it

---
+++ app.js
@@ -0,0 +1,61 @@
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
+  randomNumGen: function(minCust, maxCust) {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  }
+(firstAndPike.randomNumGen() * firstAndPike.avgCookieSale);
+};
+
+
+
+  //this.randomNumGen * this.avgCookieSale
+
+
+// var seaTacAirport = {
+//   minCust: 3,
+//   maxCust: 24,
+//   avgCookieSale: 1.2,
+//
+//   randomNumGen: function (minCust, maxCust) {
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+//   },
+//
+// };
+//
+// var seattleCenter = {
+//   minCust: 11,
+//   maxCust: 38,
+//   avgCookieSale: 3.7,
+//
+//   randomNumGen: function (minCust, maxCust) {
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+//   },
+//
+// };
+//
+// var capitolHill = {
+//   minCust: 20,
+//   maxCust: 38,
+//   avgCookieSale: 2.3,
+//
+//   randomNumGen: function (minCust, maxCust) {
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+//   },
+//
+//   randomNumGen: function
+// };
+//
+// var Alki = {
+//   minCust: 2,
+//   maxCust: 16,
+//   avgCookieSale: 4.6,
+//
+//   randomNumGen: function (minCust, maxCust) {
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+//   },
+//
+// };
+//
+//
