9d2f210c2addf16a6a634deca8b06fd10f7438be
alexanderlamoureux@hotmail.com <alexanderlamoureux@hotmail.com>
2017-05-17 19:04:06 -0700

    day 2 finally finished

--- app.js
+++ app.js
@@ -1,76 +1,106 @@
 'use strict';
-var firstAndPike = {
-  name: '1st And Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgCookieSale: 6.3,
-};
+// var firstAndPike = {
+//   name: '1st And Pike',
+//   minCust: 23,
+//   maxCust: 65,
+//   avgCookieSale: 6.3,
+// };
 
-var seaTacAirport = {
-  name: 'SeaTac Airport',
-  minCust: 3,
-  maxCust: 24,
-  avgCookieSale: 1.2,
-};
-
-var seattleCenter = {
-  name: 'Seattle Center',
-  minCust: 11,
-  maxCust: 38,
-  avgCookieSale: 3.7,
-};
-
-var capitolHill = {
-  name: 'Capitol Hill',
-  minCust: 20,
-  maxCust: 38,
-  avgCookieSale: 2.3,
-};
-
-var Alki = {
-  name: 'Alki',
-  minCust: 2,
-  maxCust: 16,
-  avgCookieSale: 4.6,
-};
-
-function randomNumGen(minCust, maxCust, avg) {
-  var people = Math.floor(Math.random() * (maxCust - minCust)) + minCust;
-  return people * avg;
+function Store(storeName, minCust, maxCust, avgCookieSale){
+  this.storeName = storeName;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookieSale = avgCookieSale;
+  this.hourly = [];
 }
 
-function hour(eachStore) {
-  var hourly = [];
+Store.prototype.cookiePerHour = function () {
+  var people = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  return people * this.avgCookieSale;
+};
 
+
+
+
+// var seaTacAirport = {
+//   name: 'SeaTac Airport',
+//   minCust: 3,
+//   maxCust: 24,
+//   avgCookieSale: 1.2,
+// };
+//
+// var seattleCenter = {
+//   name: 'Seattle Center',
+//   minCust: 11,
+//   maxCust: 38,
+//   avgCookieSale: 3.7,
+// };
+//
+// var capitolHill = {
+//   name: 'Capitol Hill',
+//   minCust: 20,
+//   maxCust: 38,
+//   avgCookieSale: 2.3,
+// };
+//
+// var Alki = {
+//   name: 'Alki',
+//   minCust: 2,
+//   maxCust: 16,
+//   avgCookieSale: 4.6,
+// };
+
+Store.prototype.hourlySales = function() {
   var total = 0;
 
-  for (var i = 0; i < 15; i++) {
-    var cookiehour = Math.floor(randomNumGen(eachStore.minCust, eachStore.maxCust, eachStore.avgCookieSale));
-    hourly.push(cookiehour);
 
+  for (var i = 0; i < 15; i++) {
+    var cookiehour = Math.floor(this.cookiePerHour());
+    this.hourly.push(cookiehour);
     total = total + cookiehour;
     console.log('thishour:',total);
-
   }
+
+
   console.log('total:',total);
 
 
-  var list = document.createElement('ul');
-  var list_arr = [];
+  // var list = document.createElement('ul');
+  // var list_arr = [];
+  //
+  // for (var b=0; b < 15; b++) {
+  //   list_arr.push('<li>' + hourly[b] + '</li>');
+  // }
 
-  for (var b=0; b < hourly.length; b++) {
-    list_arr.push('<li>' + hourly[b] + '</li>');
+  // list_arr.unshift('<li>' + eachStore.storeName + '</li>');
+  // list_arr.push('<li> total: ' + total + '</li>');
+  // var full_list = list_arr.join('');
+  // list.innerHTML = full_list;
+  // document.body.appendChild(list);
+
+  var table = document.getElementById('shell');
+  var data = ['<td>' + this.storeName + '</td>' ];
+  for (i = 0; i < this.hourly.length; i++) {
+    data.push('<td>' + this.hourly[i] + '</td>');
   }
-  list_arr.unshift('<li>' + eachStore.name + '</li>');
-  list_arr.push('<li> total: ' + total + '</li>');
-  var full_list = list_arr.join('');
+  console.log('data:', data);
 
-  list.innerHTML = full_list;
-  document.body.appendChild(list);
-}
+  data.push('<td>' + total + '</td>');
 
-hour(firstAndPike);
-hour(seaTacAirport);
-hour(seattleCenter);
-hour(capitolHill);
-hour(Alki);
+  var new_row = document.createElement('tr');
+  new_row.innerHTML = data.join('');
+  table.appendChild(new_row);
+
+};
+
+var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
+var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+var Alki = new Store('Alki', 2, 16, 4.6);
+
+firstAndPike.hourlySales();
+seaTacAirport.hourlySales();
+seattleCenter.hourlySales();
+capitolHill.hourlySales();
+Alki.hourlySales();
